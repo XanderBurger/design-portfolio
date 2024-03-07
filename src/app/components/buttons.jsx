35f49f3a43ad0d77353c346dea9adbbc1036fdf0
XanderBurger <86130720+XanderBurger@users.noreply.github.com>
2024-03-07 08:56:37 -0700

    initial commit, added framework

---
+++ src/app/components/buttons.jsx
@@ -0,0 +1,7 @@
+export default function Buttons({name, orientation}) {
+  return (
+    <div>
+        {name}
+    </div>
+  )
+}
---
+++ src/app/components/header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="grid border-b-[1px] border-solid border-black w-screen h-[80px]">
+        header
+    </div>
+  )
+}
