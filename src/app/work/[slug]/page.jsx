258fa1bd9d8513335fe5c2f656c47777d47391d2
XanderBurger <86130720+XanderBurger@users.noreply.github.com>
2024-03-20 13:47:25 -0600

    maybe this will work

--- src/app/work/[slug]/page.jsx
+++ src/app/work/[slug]/page.jsx
@@ -5,9 +5,8 @@ import Image from 'next/image'
 
 export default function Work({params}) {
    
-   const images = getWorkImages(params.slug)
-   console.log(images)
-  
+      const images = getWorkImages(params.slug)
+
   return (
     <div className='mt-[100px] mx-[40px]'>
     <h1 className='uppercase pb-4 text-hot-pink text-[42px]'>sevileta nwr</h1>
@@ -19,8 +18,8 @@ export default function Work({params}) {
            className='min-w-[500px]'
            width={500}
            height={400}
-           key={i.src} 
-           src={i.src}
+           key={i} 
+           src={i}
            />
            </div>
         )
--- src/app/work/getImages.js
+++ src/app/work/getImages.js
@@ -1,15 +1,9 @@
-import mainSev from "@/app/assets/images/sev/sevProjectCorrected70.png"
-import sketch1 from "@/app/assets/images/sev/sevSketch1.png"
-import sketch2 from "@/app/assets/images/sev/sevSketch2.png"
-import sketch3 from "@/app/assets/images/sev/sevSketch3.png"
-
 
 //get images by route slug
 const getWorkImages = (slug) =>{
    const images = {
-    "sev" : [mainSev, sketch1, sketch2, sketch3]
+    "sev" : ["/sev/sevProjectCorrected70.png", "/sev/sevSketch1.png", "/sev/sevSketch2.png", "/sev/sevSketch3.png"]
    }
-
    return (images[slug])
 }
 
